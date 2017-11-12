5c03d881e4030d2cc8b89b605dcde1d7169fed92
Nazmus Saadat <nuhin13@Nazmuss-Mac.local>
2017-11-12 16:45:44 +0600

    3rd

--- IosTest/ViewController.swift
+++ IosTest/ViewController.swift
@@ -9,10 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +19,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboard){
+        
+    }
 
 }
 
